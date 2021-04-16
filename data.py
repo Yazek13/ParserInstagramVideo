91382d1e2bed941e3e45a30e142a8ee67a03b99a
Yazek13 <fedorov_d_l@mail.ru>
2021-04-16 16:46:58 +1000

    I rewrote the code, I will make edits!

--- data.py
+++ data.py
@@ -1,2 +1,16 @@
 username = "sersemen50"
 password = "Fd9182736c"
+
+direct_users_list = ['user1']
+
+users_settings_dict = {
+    'user1': {
+        'login': 'bot_login',
+        'password': 'bot_password'
+    }
+    # 'user2': {
+    #     'login': 'bot_login',
+    #     'password': 'bot_password'
+        }
+
+
