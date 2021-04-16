ad07a9591a651b0bf5cd35a72f65bf83a52a3ce0
Yazek13 <fedorov_d_l@mail.ru>
2021-04-16 17:21:26 +1000

    test

--- data.py
+++ data.py
@@ -1,12 +1,12 @@
-username = "sersemen50"
-password = "Fd9182736c"
+username = ""
+password = ""
 
 direct_users_list = ['user1']
 
 users_settings_dict = {
     'user1': {
-        'login': 'sersemen50',
-        'password': 'Fd9182736c'
+        'login': '',
+        'password': ''
     }
     # 'user2': {
     #     'login': 'bot_login',
